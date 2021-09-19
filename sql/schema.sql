6999a714043e2458b337673f85bc29541d74143f
Andrew <64823781+AndeluPhan@users.noreply.github.com>
2021-09-18 22:25:24 -0400

    Revert "setting up virtual env"

--- sql/schema.sql
+++ sql/schema.sql
@@ -4,8 +4,4 @@ CREATE TABLE users(
   username VARCHAR(20) NOT NULL,
   fullname VARCHAR(40) NOT NULL,
   PRIMARY KEY(username)
-<<<<<<< HEAD
 );
-=======
-);
->>>>>>> 0dd2d62a941ecc2d80b1b95a43930e4fc1e60cd9
