cbc984c37b5d106882867c860eb09b55ba1f8287
Andrew Huang <andrewhuang389@gmail.com>
2021-09-20 18:27:41 -0400

    render correct login

--- insta485/views/index.py
+++ insta485/views/index.py
@@ -15,15 +15,17 @@ def show_index():
         return flask.redirect(flask.url_for('show_login'), code=200) #302
 
     # Connect to database
-    connection = insta485.model.get_db()
+    # connection = insta485.model.get_db()
 
     # Query database
-    cur = connection.execute(
-        "SELECT username, fullname "
-        "FROM users"
-    )
-    users = cur.fetchall()
+    # cur = connection.execute(
+    #     "SELECT username, fullname "
+    #     "FROM users"
+    # )
+    # users = cur.fetchall()
 
     # Add database info to context
-    context = {"users": users}
-    return flask.render_template("index.html", **context)
+    # context = {"users": users}
+
+    # return flask.render_template("index.html", **context)
+    return flask.render_template("index.html")
