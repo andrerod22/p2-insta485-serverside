b8fa8ddceda453156c29253c4d610353bd9adde0
Andrew Huang <andrewhuang389@gmail.com>
2021-09-19 23:25:32 -0400

    user authentication work-in-progress

--- insta485/views/accounts.py
+++ insta485/views/accounts.py
@@ -9,6 +9,7 @@ URLs include:
 /password/
 / immediate redirection to where the user originally wanted to go. 
 """
+from os import abort
 import flask
 import insta485
 
@@ -17,8 +18,17 @@ def account_redirect():
     target = flask.request.args.get('target')
     if target == '':
         operation = flask.request.form['operation']
+        username = flask.request.form['username']
+        password = flask.request.form['password']
+        if not username or not password:
+            abort(400, "Username or Password field was empty")
         if operation == 'login':
-            #instantialize session variables. 
+            connection = insta485.model.get_db()
+            sql =  "SELECT username FROM users WHERE username = %s AND password = %s"
+            cur = connection.execute(sql, (username, password))
+            user = cur.fetchone()
+            if user is None:
+                flask.abort(403, "Invalid Username and Password Combination")
             return flask.redirect("/", code=302)
 
 @insta485.app.route('/accounts/login', methods=["GET"])
