b1bc78e123455cdb4dcc943625d34cbdd2a4f59c
Danyelpaladin <zongxih@umich.edu>
2021-09-23 01:52:33 -0400

    Added explore.html and follow.py

---
+++ insta485/views/follow.py
@@ -0,0 +1,34 @@
+"""
+Insta485 logout
+"""
+from os import abort
+import flask
+import insta485
+
+@insta485.app.route('/explore/', methods=["POST"])
+def update_follow(followUser):
+    currUser = flask.session['username']
+    target = flask.request.args.get('target')
+    operation = flask.request.form['operation']
+    follow_tar = flask.request.form['username']
+    connection = insta485.model.get_db()
+    sql = "SELECT username2 FROM following WHERE username1='%s' AND username='%s'" % (currUser, follow_tar)
+    cur = connection.execute(sql)
+    followData = cur.fetchall()
+
+    if operation == follow:
+        if followData:
+            flask.abort(409, "User cannot follow someone they already follow")
+        time_stamp = datetime.datetime.utcnow()
+        time_stamp = time_stamp.strftime('%Y-%m-%d %H:%M:%S')
+        sql = "INSERT INTO following (username1, username2, created) VALUES '%s', '%s', '%s'" % (currUser, follow_tar, time_stamp)
+        cur = connection.execute(sql)
+    else:
+        if followData:
+            sql = "DELETE FROM following WHERE username1='%s' AND username2='%s'" % (currUser, follow_tar)
+            cur = connection.execute(sql)
+        else:
+            flask.abort(409, "User cannot unfollow someone they do not follow")
+        
+
+                    
