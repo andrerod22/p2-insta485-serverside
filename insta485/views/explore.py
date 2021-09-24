13d2cd81a26d65d7a4078fb9eb8167a9e124cbd4
Andre Rodriguez <andre.rodriguez9722@outlook.com>
2021-09-23 22:19:38 -0400

    Post page added dynamically.

--- insta485/views/explore.py
+++ insta485/views/explore.py
@@ -5,7 +5,7 @@ import insta485
 def show_explore():
     currUser = flask.session['username']
     connection = insta485.model.get_db() #username is a primary key. 
-    sql = "SELECT username FROM users WHERE username NOT in (SELECT username2 FROM following WHERE username1='%s')  % currUser="
+    sql = "SELECT username FROM users WHERE username NOT in (SELECT username2 FROM following WHERE username1='%s')"  % (currUser)
     #Select username2 from following where username1 does not follow username2
     #  
     cur = connection.execute(sql)
--- insta485/views/post.py
+++ insta485/views/post.py
@@ -1,16 +1,36 @@
 import flask
 import insta485
 import arrow
-import pdb
-@insta485.app.route("/posts/<postid_url_slug>", methods=["GET"])
+
+@insta485.app.route("/posts/<postid_url_slug>/", methods=["GET"])
 def show_post(postid_url_slug):
-    breakpoint()
     currUser = flask.session['username']
     connection = insta485.model.get_db() #username is a primary key. 
-    sql = "SELECT postid, filename, owner, created FROM posts (WHERE owner='%s' OR owner in (SELECT username2 FROM following WHERE username1='%s') AND postid = postid_url_slug) ORDER BY postid DESC" % (currUser, currUser)
+    sql = "SELECT postid, filename, owner, created FROM posts WHERE postid='%s'" % postid_url_slug
     cur = connection.execute(sql)
     postData = cur.fetchall()
     for post in postData:
         post['created'] = arrow.get(post['created'], 'YYYY-MM-DD HH:mm:ss').humanize()
+    sql = "SELECT c.postid, c.commentid, c.owner, c.text, c.created FROM comments AS c INNER JOIN (SELECT * FROM posts WHERE postid='%s') AS p ON (p.postid = c.postid)" % postid_url_slug
+    cur = connection.execute(sql)
+    commentData = cur.fetchall()
+    sql = "SELECT l.postid, l.likeid, l.owner, l.created FROM likes AS l INNER JOIN (SELECT * FROM posts WHERE postid='%s') AS p ON (p.postid = l.postid)" % postid_url_slug
+    cur = connection.execute(sql)
+    likeData = cur.fetchall()
+    for p in postData:
+        commentTuple = []
+        likes = 0
+        liked = False
+        for c in commentData:
+            if p["postid"] == c["postid"]:
+                commentTuple.append(c)
+        for l in likeData:
+            if p["postid"] == l["postid"]:
+                if(currUser == l["owner"]):
+                    liked = True
+                likes += 1
+        p["comments"] = commentTuple
+        p["likes"] = likes
+        p["liked"] = liked
     context = {"posts": postData}
     return flask.render_template("post.html", **context)
