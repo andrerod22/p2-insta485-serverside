b15dcf23eb9814b1bfbe73e51ce15203f0d80e06
Andre Rodriguez <andre.rodriguez9722@outlook.com>
2021-09-23 23:52:11 -0400

    Grabbed all userProfile data, implementing on user.html in progress.

--- insta485/views/users.py
+++ insta485/views/users.py
@@ -3,22 +3,23 @@ import insta485
 
 @insta485.app.route('/users/<user_slug>/', methods=["GET"])
 def show_user(user_slug):
-    currUser = flask.session['username']
-    sql = "SELECT * From users WHERE username='%s'" % (currUser)
+    #currUser = flask.session['username']
+    sql = "SELECT * FROM users WHERE username='%s'" % (user_slug)
     connection = insta485.model.get_db()
     cur = connection.execute(sql)
-    userData = cur.fetchall()
-    sql = "SELECT COUNT(postid) FROM posts"
+    userData = cur.fetchall() # LIST, should be
+    sql = "SELECT postid FROM posts WHERE owner='%s'" % (user_slug)
     cur = connection.execute(sql)
-    userData['numPosts'] = cur.fetchall()
-    sql = "SELECT COUNT(username2) FROM following WHERE username1='%s'" % (currUser)
+    userData[0]["numPosts"] = len(cur.fetchall())
+    sql = "SELECT * FROM following WHERE username1='%s'" % (user_slug)
     cur = connection.execute(sql)
-    userData['numFollowing'] = cur.fetchall()
-    sql = "SELECT COUNT(username1) FROM following WHERE username2='%s'" % (currUser)
+    userData[0]['numFollowing'] = len(cur.fetchall())
+    sql = "SELECT * FROM following WHERE username2='%s'" % (user_slug)
     cur = connection.execute(sql)
-    userData['numFollowers'] = cur.fetchall()
+    userData[0]['numFollowers'] = len(cur.fetchall())
     #ssql = "SELECT fullname FROM users WHERE username='%s'" % (currUser)
     #cur = connection.execute(sql)
     #userData['fullname'] = cur.fetchall()
     context = {"UserData": userData} 
+    #breakpoint()
     return flask.render_template("user.html", **context)
