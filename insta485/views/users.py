156aed3448da2d56e9e1a511fcb099ddf986858c
Danyelpaladin <zongxih@umich.edu>
2021-09-23 21:56:55 -0400

    Added experimental user.html and user.py

--- insta485/views/users.py
+++ insta485/views/users.py
@@ -3,7 +3,20 @@ import insta485
 
 @insta485.app.route('/users/<user_slug>/', methods=["GET"])
 def show_user(user_slug):
-
-    
-    #Connect to database and fully render this page. 
-    return flask.render_template("user.html")
+    currUser = flask.session['username']
+    connection = insta485.model.get_db()
+    sql = "SELECT COUNT(postid) FROM posts"
+    cur = connection.execute(sql)
+    userData['numPosts'] = cur.fetchall()
+    sql = "SELECT COUNT(username2) FROM following WHERE username1='%s'" % (currUser)
+    cur = connection.execute(sql)
+    userData['numFollowing'] = cur.fetchall()
+    sql = "SELECT COUNT(username1) FROM following WHERE username2='%s'" % (currUser)
+    cur = connection.execute(sql)
+    userData['numFollowers'] = cur.fetchall()
+    sql = "SELECT fullname FROM users WHERE username='%s'" % (currUser)
+    cur = connection.execute(sql)
+    userData['fullname'] = cur.fetchall()    
+    #Connect to database and fully render this page.
+    context = {"UserData": userData} 
+    return flask.render_template("user.html", **context)
