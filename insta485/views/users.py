af842c5ccf1d23a44a152bfac850e3d85a13f4c7
Danyelpaladin <zongxih@umich.edu>
2021-09-23 22:39:08 -0400

    Fixed errors in users.py and user.html

--- insta485/views/users.py
+++ insta485/views/users.py
@@ -4,7 +4,10 @@ import insta485
 @insta485.app.route('/users/<user_slug>/', methods=["GET"])
 def show_user(user_slug):
     currUser = flask.session['username']
+    sql = "SELECT * From users WHERE username='%s'" % (currUser)
     connection = insta485.model.get_db()
+    cur = connection.execute(sql)
+    userData = cur.fetchall()
     sql = "SELECT COUNT(postid) FROM posts"
     cur = connection.execute(sql)
     userData['numPosts'] = cur.fetchall()
@@ -14,9 +17,8 @@ def show_user(user_slug):
     sql = "SELECT COUNT(username1) FROM following WHERE username2='%s'" % (currUser)
     cur = connection.execute(sql)
     userData['numFollowers'] = cur.fetchall()
-    sql = "SELECT fullname FROM users WHERE username='%s'" % (currUser)
-    cur = connection.execute(sql)
-    userData['fullname'] = cur.fetchall()    
-    #Connect to database and fully render this page.
+    #ssql = "SELECT fullname FROM users WHERE username='%s'" % (currUser)
+    #cur = connection.execute(sql)
+    #userData['fullname'] = cur.fetchall()
     context = {"UserData": userData} 
     return flask.render_template("user.html", **context)
