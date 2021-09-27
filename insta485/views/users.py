4d618c5c53b10f5c3f3bbfd0b9cbb0623e67e397
Andrew Huang <andrewhuang389@gmail.com>
2021-09-26 20:33:56 -0400

    style users.py

--- insta485/views/users.py
+++ insta485/views/users.py
@@ -4,13 +4,13 @@ import pathlib
 import uuid
 import datetime
 
+
 @insta485.app.route('/users/<user_slug>/', methods=["GET"])
 def show_user(user_slug):
     if 'username' not in flask.session:
         return flask.redirect('show_login')
     currUser = flask.session['username']
     connection = insta485.model.get_db()
-    #Check if user_slug is in the database
     sql = "SELECT username FROM users"
     cur = connection.execute(sql)
     checkUser = cur.fetchall()
@@ -20,16 +20,14 @@ def show_user(user_slug):
             is_in_db = True
     if is_in_db is False:
         flask.abort(404, "User_slug was not found in DB")
-    #retrieve info for user.html
-    #Grab visited user's username:
     sql = "SELECT * FROM users WHERE username='%s'" % (user_slug)
     cur = connection.execute(sql)
-    userData = cur.fetchall() # LIST, should be
-    #Grab all posts user made:
+    userData = cur.fetchall()
+    # Grab all posts user made:
     sql = "SELECT postid FROM posts WHERE owner='%s'" % (user_slug)
     cur = connection.execute(sql)
     userData[0]["numPosts"] = len(cur.fetchall())
-    #Get owner's profile picture:
+    # Get owner's profile picture:
     sql = "SELECT filename FROM posts WHERE owner='%s'" % (user_slug)
     cur = connection.execute(sql)
     userData[0]['post_img'] = cur.fetchall()
@@ -40,17 +38,18 @@ def show_user(user_slug):
     for post in userData[0]['post_img']:
         post['post_id'] = post_ids[i]['postid']
         i += 1
-    
-    #Get number of following:
+
+    # Get number of following:
     sql = "SELECT * FROM following WHERE username1='%s'" % (user_slug)
     cur = connection.execute(sql)
     userData[0]['numFollowing'] = len(cur.fetchall())
-    sql = "SELECT username2 FROM following WHERE username1='%s' AND username2='%s'" % (currUser, user_slug)
+    params = (currUser, user_slug)
+    sql = """SELECT username2 FROM following
+        WHERE username1='%s' AND username2='%s'""" % params
     cur = connection.execute(sql)
     userData[0]['relation'] = cur.fetchall()
     sql = "SELECT * FROM following WHERE username2='%s'" % (user_slug)
     cur = connection.execute(sql)
     userData[0]['numFollowers'] = len(cur.fetchall())
     context = {"UserData": userData}
-    #breakpoint()
     return flask.render_template("user.html", **context)
